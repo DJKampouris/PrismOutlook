bc46cf5635d50e93ebcd3308ec3dc6d8471a7d13
Dimitri Hudon-Kampouris <dimitri@binex.com>
2019-12-02 13:29:43 -0500

    Auto stash before revert of "Merge branch 'patch-1' into localinstall"

--- PrismOutlook/Core/Dialogs/RegionDialogService.cs
+++ PrismOutlook/Core/Dialogs/RegionDialogService.cs
@@ -6,6 +6,7 @@ using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace PrismOutlook.Core.Dialogs
 {
